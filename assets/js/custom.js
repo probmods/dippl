b9d7a4903aa06347a4fc04f407404c26c13c8543
Andreas Stuhlmueller <andreas@stuhlmueller.org>
2014-08-08 14:21:03 -0700

    webppl code boxes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,22 +1,50 @@
-
 // Code boxes
+
+var codeBoxCount = 0;
 
 function setupCodeBoxes(){
     $('pre > code').each(function() {
-        
-        var $this = $(this),
+
+      var $this = $(this),
         $code = $this.html(),
         $unescaped = $('<div/>').html($code).text();
-        
+
         $this.empty();
 
-        CodeMirror(this, {
-            value: $unescaped,
-            mode: 'javascript',
-            lineNumbers: false,
-            readOnly: false
+        var cm = CodeMirror(this, {
+          value: $unescaped,
+          mode: 'javascript',
+          lineNumbers: false,
+          readOnly: false
         });
-        
+
+        var resultDiv = $('<div/>', {
+          "class": "resultDiv"
+        });
+
+        var showResult = function(x){
+          resultDiv.show();
+          resultDiv.text(x);
+        };
+
+        var runButton = $('<button/>', {
+          "text": "run",
+          "id": 'run_' + codeBoxCount,
+          "class": 'runButton',
+          "click": function () {
+              try {
+                webppl.run(cm.getValue(), showResult, false);
+              } catch (err) {
+                showResult(err.message);
+              }
+          }
+        });
+
+        $this.parent().append(resultDiv);
+        $this.parent().append(runButton);
+
+        codeBoxCount += 1;
+
     });
 }
 
