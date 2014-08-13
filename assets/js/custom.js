47cf09bdf2242cee3752c30a365f3cfe2680fa2c
Andreas Stuhlmueller <andreas@stuhlmueller.org>
2014-08-13 13:36:48 -0700

    Add graphics particle filter (currently hampered by non-log probs in particle filter and by small stack size limit in browser)

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,43 +1,108 @@
 "use strict";
 
 var topK; // Top-level continuation
+var activeCodeBox;
+
+
+// Utils
+
+function euclideanDistance(v1, v2){
+  var i;
+  var d = 0;
+  for (i = 0; i < v1.length; i++) {
+    d += (v1[i] - v2[i])*(v1[i] - v2[i]);
+  }
+  return Math.sqrt(d);
+};
+
 
 
 // Drawing
 
-var activeCanvas = undefined;
+function DrawObject(width, height, visible){
+  this.canvas = $('<canvas/>', {
+    "class": "drawCanvas",
+    "Width": width + "px",
+    "Height": height + "px"
+  })[0];
+  if (visible==true){
+    $(this.canvas).css({"display": "inline"});
+    $(activeCodeBox).append(this.canvas);
+  };
+  this.paper = new paper.PaperScope();
+  this.paper.setup(this.canvas);
+  this.paper.view.viewSize = new this.paper.Size(width, height);
+  this.redraw();
+}
 
-var Draw = {
-
-  newPath: function() {
-    var path = new paper.Path(); // .apply(this, arguments);
-    path.strokeColor = 'black';
-    path.strokeWidth = 4;
-    path.opacity = 0.6;
-    return path;
-  },
-
-  newPoint: function(x, y) {
-    return new paper.Point(x, y); // .apply(this, arguments);
-  },
-
-  line: function(x1, y1, x2, y2){
-    var path = this.newPath();
-    path.moveTo(x1, y1);
-    path.lineTo(this.newPoint(x2, y2));
-    this.redraw();
-  },
-
-  setup: function(){
-    $(activeCanvas).css( "display", "block");
-    paper.setup(activeCanvas);
-  },
-
-  redraw: function(){
-    paper.view.draw();
-  }
-
+DrawObject.prototype.newPath = function(){
+  var path = new this.paper.Path();
+  path.strokeColor = 'black';
+  path.strokeWidth = 8;
+  path.opacity = 0.6;
+  return path;
 };
+
+DrawObject.prototype.newPoint = function(x, y){
+  return new this.paper.Point(x, y);
+};
+
+DrawObject.prototype.line = function(x1, y1, x2, y2){
+  var path = this.newPath();
+  path.moveTo(x1, y1);
+  path.lineTo(this.newPoint(x2, y2));
+  this.redraw();
+};
+
+DrawObject.prototype.redraw = function(){
+  this.paper.view.draw();
+};
+
+DrawObject.prototype.toArray = function(){
+  var context = this.canvas.getContext('2d');
+  var imgData = context.getImageData(0, 0, this.canvas.width, this.canvas.height);
+  return imgData.data;
+};
+
+DrawObject.prototype.distance = function(cmpDrawObject){
+  if (!((this.canvas.width == cmpDrawObject.canvas.width) &&
+        (this.canvas.height == cmpDrawObject.canvas.height))){
+    console.log(this.canvas.width, cmpDrawObject.canvas.width,
+                this.canvas.height, cmpDrawObject.canvas.height);
+    throw new Error("Dimensions must match for distance computation!");
+  }
+  var thisImgData = this.toArray();
+  var cmpImgData = cmpDrawObject.toArray();
+  var distance = 0;
+  for (var i=0; i<thisImgData.length; i+=4) {
+    var col1 = [thisImgData[i], thisImgData[i+1], thisImgData[i+2], thisImgData[i+3]];
+    var col2 = [cmpImgData[i], cmpImgData[i+1], cmpImgData[i+2], cmpImgData[i+3]];
+    distance += euclideanDistance(col1, col2);
+  };
+  return distance;
+};
+
+DrawObject.prototype.destroy = function(){
+  this.paper = undefined;
+  $(this.canvas).remove();
+}
+
+function Draw(k, width, height, visible){
+  return k(new DrawObject(width, height, visible));
+}
+
+function loadImage(k, drawObject, url){
+  // Synchronous loading - only continue with computation once image is loaded
+  var context = drawObject.canvas.getContext('2d');
+  var imageObj = new Image();
+  imageObj.onload = function() {
+    var raster = new drawObject.paper.Raster(imageObj);
+    raster.position = drawObject.paper.view.center;
+    drawObject.redraw();
+    k();
+  };
+  imageObj.src = url;
+}
 
 
 // Code boxes
@@ -47,61 +112,55 @@ var codeBoxCount = 0;
 function setupCodeBoxes(){
   $('pre > code').each(function() {
 
-    var $this = $(this),
-      $code = $this.html(),
-      $unescaped = $('<div/>').html($code).text();
+    var $this = $(this);
+    var $code = $this.html();
+    var $unescaped = $('<div/>').html($code).text();
 
-      $this.empty();
+    $this.empty();
 
-      var cm = CodeMirror(this, {
+    var cm = CodeMirror(
+      this, {
         value: $unescaped,
         mode: 'javascript',
         lineNumbers: false,
         readOnly: false
       });
 
-      var resultDiv = $('<div/>', {
-        "class": "resultDiv"
-      });
+    var resultDiv = $('<div/>', { "class": "resultDiv" });
 
-      var resultCanvas = $('<canvas/>', {
-        "class": "resultCanvas",
-        "width": 200,
-        "height": 200
-      })[0];
+    var showResult = function(x){
+      resultDiv.show();
+      resultDiv.text(x);
+    };
 
-      var showResult = function(x){
-        resultDiv.show();
-        resultDiv.text(x);
-      };
-
-      var runButton = $('<button/>', {
+    var runButton = $(
+      '<button/>', {
         "text": "run",
         "id": 'run_' + codeBoxCount,
         "class": 'runButton',
         "click": function () {
-          var oldActiveCanvas = activeCanvas;
           var oldTopK = topK;
-          activeCanvas = resultCanvas;
+          var oldActiveCodeBox = activeCodeBox;
           topK = showResult;
+          activeCodeBox = $this;
+          activeCodeBox.find("canvas").remove();
           try {
             var compiled = webppl.compile(cm.getValue(), true);
             eval.call(window, compiled);
-          } catch (err) {
-            showResult(err.message);
-            throw err;
+            // } catch (err) {
+            //   showResult(err.message);
+            //   throw err;
           } finally {
-            activeCanvas = oldActiveCanvas;
-            topK = oldTopK;
+            // topK = oldTopK;
+            // activeCodeBox = oldActiveCodeBox;
           }
         }
       });
 
-      $this.parent().append(resultDiv);
-      $this.parent().append(resultCanvas);
-      $this.parent().append(runButton);
+    $this.parent().append(resultDiv);
+    $this.parent().append(runButton);
 
-      codeBoxCount += 1;
+    codeBoxCount += 1;
 
   });
 }
--- lectures/graphics.md
+++ lectures/graphics.md
@@ -7,18 +7,42 @@ description: Render to canvas, condition on output
 Drawing a line:
 
 ~~~~
-Draw.setup()
+var myDraw = Draw(200, 200, true);
 var x1 = randomInteger(200);
 var y1 = randomInteger(200);    
 var x2 = randomInteger(200);
 var y2 = randomInteger(200);        
-Draw.line(x1, y1, x2, y2);
+myDraw.line(x1, y1, x2, y2);
+~~~~
+
+Loading an image (synchronously):
+
+~~~~
+var myDraw = Draw(200, 200, true);
+loadImage(myDraw, "/esslli2014/assets/img/the_scream.jpg");
+~~~~
+
+Computing the distance between two images:
+
+~~~~
+var imageCanvas = Draw(200, 200, true);
+loadImage(imageCanvas, "/esslli2014/assets/img/the_scream.jpg")
+
+var lineCanvas = Draw(200, 200, true);
+loadImage(lineCanvas, "/esslli2014/assets/img/the_scream.jpg")
+var x1 = randomInteger(200);
+var y1 = randomInteger(200);    
+var x2 = randomInteger(200);
+var y2 = randomInteger(200);        
+lineCanvas.line(x1, y1, x2, y2);
+
+imageCanvas.distance(lineCanvas);
 ~~~~
 
 Drawing many lines:
 
 ~~~~
-Draw.setup()
+var myDraw = Draw(200, 200, true);
 
 var makeLines = function(n, xs){
   var x1 = randomInteger(200);
@@ -30,10 +54,47 @@ var makeLines = function(n, xs){
 }
 
 var drawLines = function(lines){
-  return (lines.length == 0) ? "done" : drawLines(lines.slice(1));
+  var line = lines[0];
+  myDraw.line(line[0], line[1], line[2], line[3]);
+  return (lines.length == 1) ? "done" : drawLines(lines.slice(1));
 }
 
 var lines = makeLines(100, []);
 
 drawLines(lines);
+~~~~
+
+Inferring lines that match an image:
+
+~~~~
+var targetImage = Draw(50, 50, true);
+loadImage(targetImage, "/esslli2014/assets/img/box.png")
+
+var drawLines = function(drawObj, lines){
+  var line = lines[0];
+  drawObj.line(line[0], line[1], line[2], line[3]);
+  return (lines.length == 1) ? "done" : drawLines(drawObj, lines.slice(1));
+}
+
+var makeLines = function(n, lines){
+  var x1 = randomInteger(50);
+  var y1 = randomInteger(50);    
+  var x2 = randomInteger(50);
+  var y2 = randomInteger(50);        
+  var newLines = lines.concat([[x1, y1, x2, y2]]);
+  var generatedImage = Draw(50, 50, false);
+  drawLines(generatedImage, newLines);
+  var score = -targetImage.distance(generatedImage)/100000;
+  console.log(score);
+  factor(score);
+  generatedImage.destroy();
+  return (n==0) ? newLines : makeLines(n-1, newLines);
+}
+
+ParticleFilter(
+  function(){
+    var lines = makeLines(4, []);
+    var finalGeneratedImage = Draw(50, 50, true);
+    drawLines(finalGeneratedImage, lines);
+   }, 10)
 ~~~~
