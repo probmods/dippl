a3436ab3f32fa22b43f017893d1954e420b088df
Andreas Stuhlmüller <andreas@stuhlmueller.org>
2016-06-15 12:56:28 -0700

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,5 @@
 "use strict";
 
-var activeCodeBox;
-
 // Utils
 
 function euclideanDistance(v1, v2){
@@ -14,28 +12,29 @@ function euclideanDistance(v1, v2){
 };
 
 
-
 // Code boxes
+
 function setupCodeBoxes(){
   // TODO: optimize this, (maybe have wpEditor.setup take a content option?)
   $("pre:not(#bibtex)").map(function(i,el) {
     var firstLine = $(el).text().split("\n")[0];
     var language = (firstLine == '// language: javascript' ? 'javascript' : 'webppl');
-    wpEditor.setup(el, {language: language})
-  })
+    wpEditor.setup(el, {language: language});    
+  });
 }
 
 $(setupCodeBoxes);
 
+
 // Google analytics
 
-// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-// (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-// m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-// })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-// ga('create', 'UA-54996-12', 'auto');
-// ga('send', 'pageview');
+ga('create', 'UA-54996-12', 'auto');
+ga('send', 'pageview');
 
 
 // Date
@@ -57,24 +56,4 @@ function setBibtex(){
   $('#toggle-bibtex').click(function(){$('#bibtex').toggle(); return false;});
 }
 
-$(setBibtex)
-
-// Special functions for webppl code boxes
-
-var invertMap = function (store, k, a, obj) {
-
-  var newObj = {};
-
-  for (var prop in obj) {
-    if (obj.hasOwnProperty(prop)) {
-      var value = obj[prop];
-      if (newObj.hasOwnProperty(value)) {
-        newObj[value].push(prop);
-      } else {
-        newObj[value] = [prop];
-      }
-    }
-  }
-
-  return k(store, newObj);
-};
+$(setBibtex);
